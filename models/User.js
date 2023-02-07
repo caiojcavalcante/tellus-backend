932682fc3b7ba7cf02638355b280336a39f3ec00
Caio Cavalcante <danielcaio75@gmail.com>
2023-02-07 16:08:39 -0300

    publish

---
+++ models/User.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: true,
+            min: 2,
+            max: 50,
+        },
+        lastName: {
+            type: String,
+            required: true,
+            min: 2,
+            max: 50,
+        },
+        email: {
+            type: String,
+            required: true,
+            max: 50,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            min: 6,
+        },
+        picturePath: {
+            type: String,
+            default: "",
+        },
+        friends: {
+            type: Array,
+            default: [],
+        },
+        location: String,
+        occupation: String,
+        viewedProfile: Number,
+        impressions: Number,
+    }, { timestamps: true });
+
+if(!mongoose.modelNames().includes('User')) {
+    mongoose.model("User", userSchema);
+}
+const User = mongoose.model("User");
+
+export default User;
+        
