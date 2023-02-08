328aea5cb9627fb797bca945b3756b27c5804cd7
Caio Cavalcante <danielcaio75@gmail.com>
2023-02-07 23:11:54 -0300

    novo update

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,6 @@ import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
 
 /* Register user */
-
 export const register = async (req, res) => {
     try {
         const {
@@ -38,7 +37,6 @@ export const register = async (req, res) => {
 };
 
 /* Login user */
-
 export const login = async (req, res) => {
     try {
         const { email, password } = req.body;
